f68af04df64d30166a139b590ed286f24fa52c73
saja-aabith <69687206+saja-aabith@users.noreply.github.com>
2024-08-11 01:08:58 +0300

    Create EncodeGenerator.py

---
+++ EncodeGenerator.py
@@ -0,0 +1,39 @@
+import cv2
+import face_recognition
+import pickle
+import os
+
+
+
+# importing student images
+folderModePath = 'Images'
+pathList = os.listdir(folderModePath)
+imgList = []
+studentIds = []
+for path in pathList:
+    img = cv2.imread(os.path.join(folderModePath, path))
+    studentIds.append(os.path.splitext(path)[0])
+    
+    
+    if img is not None:
+        imgList.append(img)
+    else:
+        print(f"Warning: Could not read image {path}")
+        
+def findEncodings(imageList):
+    
+    encodeList = []
+    for img in imageList:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+
+
+print('Encoding Started...')
+encodeListKnown = findEncodings(imgList)
+encodeListKnownWithIds = [encodeListKnown, studentIds]
+
+
+print('Encoding Complete')
