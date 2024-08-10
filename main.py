5eaa095e45ff82aa3ff6d0221b87309543ad3d2d
saja-aabith <69687206+saja-aabith@users.noreply.github.com>
2024-08-10 23:56:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import cv2
 
 cap = cv2.VideoCapture(1)
-cap.set(3,1280)
-cap.set(4,720)
+cap.set(3,640)
+cap.set(4,480)
 
 imgBackground = cv2.imread('Resources/background.png')
 
 while True:
     success, img = cap.read()
-    cv2.imshow("Camera attendance", img)
-    cv2.imshow("Face")
+    
+    # to overlay the camera and the background image at precice location in image
+    imgBackground[162:162+480, 55:55+640] = img
+    
+    # cv2.imshow("Camera G1", img)
+    cv2.imshow("Student attendance", imgBackground)
     cv2.waitKey(1)
     
 # The above code will open the camera and display the video feed in a window
